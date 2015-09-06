5232b59d21b72ca058fb4116ce17f6d803c0eef3
Calvin Gonçalves de Aquino <calving.aquino@gmail.com>
2015-09-06 13:21:51 -0300

    Base project files  - Added empty buildable project

---
+++ newsApp/newsApp/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  newsApp
+//
+//  Created by Calvin Gonçalves de Aquino on 9/6/15.
+//  Copyright (c) 2015 Calvin Gonçalves de Aquino. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
